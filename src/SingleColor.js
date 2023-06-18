84b387d22106c7349876aa9d87d4d0c94076fdf1
ShivankarMehta <78727412+ShivankarMehta@users.noreply.github.com>
2023-06-18 17:40:14 +0530

    first commit

---
+++ src/SingleColor.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import rgbToHex from './utils'
+const SingleColor = ({rgb, weight, index, hexColor}) => {
+    const [alert,setAlert]= useState(false);
+    const bcg=rgb.join(',')
+    const hex= rgbToHex(...rgb)
+  return (
+    <article className={`color ${index>10 && 'color-light'}`} style={{backgroundColor:`rgb(${bcg})`}}>
+    <p className="percent-value">{weight}%</p>
+    <p className="color-value">{hexColor}</p>
+    </article>
+  )
+}
+
+export default SingleColor
